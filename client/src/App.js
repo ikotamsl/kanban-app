3721973de6aa846b913c190dfeaacef8a559b76c
Ivan Sys <ilsimim@yandex.ru>
2022-03-11 14:20:58 +0300

    app

---
+++ client/src/App.js
@@ -0,0 +1,55 @@
+import logo from './logo.svg';
+import './App.css';
+import Board from '@asseinfo/react-kanban';
+import '@asseinfo/react-kanban/dist/styles.css';
+
+const board = {
+    columns: [
+        {
+            id: 1,
+            title: 'Backlog',
+            cards: [
+                {
+                    id: 1,
+                    title: 'Add card',
+                    description: 'Add capability to add a card in a column'
+                },
+            ]
+        },
+        {
+            id: 2,
+            title: 'Doing',
+            cards: [
+                {
+                    id: 2,
+                    title: 'Drag-n-drop support',
+                    description: 'Move a card between the columns'
+                },
+            ]
+        }
+    ]
+}
+
+function App() {
+  return (
+      <Board initialBoard={board} />
+    // <div className="App">
+    //   <header className="App-header">
+    //     <img src={logo} className="App-logo" alt="logo" />
+    //     <p>
+    //       Edit <code>src/App.js</code> and save to reload.
+    //     </p>
+    //     <a
+    //       className="App-link"
+    //       href="https://reactjs.org"
+    //       target="_blank"
+    //       rel="noopener noreferrer"
+    //     >
+    //       Learn React
+    //     </a>
+    //   </header>
+    // </div>
+  );
+}
+
+export default App;
---
+++ server/index.js
@@ -0,0 +1,3 @@
+app = require('express');
+
+app.
